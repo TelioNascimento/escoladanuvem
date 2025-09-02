71bda45e9e532eb8e6e04471270d8ab631d50aa6
TelioMauricio <telionascimento@gmail.com>
2025-09-02 12:05:27 -0300

    atividade7

--- atividade_pratica_07/escrever_csv.py
+++ atividade_pratica_07/escrever_csv.py
@@ -5,16 +5,18 @@ O arquivo CSV deve conter informações pessoais, como colunas Nome, Idade e Cid
 
 import csv
 
+
+
 def escrever_csv(nome_arquivo, dados):
     try:
-        with open (nome_arquivo, 'w') as arquivo_csv:
+        with open(nome_arquivo, 'w') as arquivo_csv:
             escritor = csv.writer(arquivo_csv)
             escritor.writerow(['Nome', 'Idade', 'Cidade'])
             for linha in dados:
                 escritor.writerow(linha)
-        print(f"Arquivo salvo com sucesso em {nome_arquivo}")
+        print(f"Arquivo salvo com sucesso em {nome_arquivo}")        
     except Exception as e:
-        print(f"Erro {e} ao salvar o arquivo. ")
+        print(f"Erro {e} ao salvar o arquivo.")
 
 dados = [
     ['Ana', 28, 'Rio de Janeiro'],
@@ -23,6 +25,9 @@ dados = [
     ['Maria', 32, 'Salvador']
 ]
 
-if __name__ == "_main__":
-    nome_arquivo = input("Digite o nome do arquivo CSV: ").strip
+
+if __name__ == "__main__":
+    nome_arquivo = input("Digite o nome do arquivo CSV: ").strip()
+    if not nome_arquivo.endswith('.csv'):
+        nome_arquivo += '.csv'
     escrever_csv(nome_arquivo, dados)
