2a750e50d64adddd1fa4a88dce31ccf0f0670b6c
TelioMauricio <telionascimento@gmail.com>
2025-09-04 10:21:10 -0300

    04092025

--- atividade_pratica_07/ler_csv.py
+++ atividade_pratica_07/ler_csv.py
@@ -1,3 +1,8 @@
+"""
+Crie um script en Python que leia um arquivo CSV e exiba os dados na tela. O arquivo CSV deve conter informações de pessoas,
+ com colunas Nome, Idade e Cidade.
+"""
+
 import csv
 def ler_csv(nome_arquivo):
     try:
